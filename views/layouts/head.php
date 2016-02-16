51fd2f2103182f8130e3144ae531a22afd8a4aa0
WebCrew <palareas@gmail.com>
2016-02-17 00:08:18 +0100

    initial version
    The first version of my theme. Already working with HumHub. Have fun
    using it!

---
+++ views/layouts/head.php
@@ -0,0 +1,3 @@
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/font/open_sans/open-sans.css'; ?>" rel="stylesheet">
+
